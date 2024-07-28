cced27e057c1b9c7f6ee9ce687ec40e382124f8b
manish <manishkrsah88@gmail.com>
2024-07-28 14:15:35 +0545

    fix menu component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -40,8 +40,24 @@ const Navbar = () => {
   const handleNav=()=>{
     setNav(!nav)
   }
+  
+    useEffect(() => {
+      const handleResize = () => {
+          if (window.innerWidth >= 768) {
+              setNav(false);
+          }
+      };
+
+      window.addEventListener('resize', handleResize);
+
+      // Cleanup event listener on component unmount
+      return () => {
+          window.removeEventListener('resize', handleResize);
+      };
+  }, []);
 
   return (
+
     <div className={`fixed w-full flex justify-between items-center px-9 p-2 text-sm font-medium ${show?'transform translate-y-0  z-10 duration-700 ' : 'transform translate-y-[-100%] bg-white text-black z-10 duration-700'} ${transparent ? 'bg-transparent' : 'bg-white text-black'}`}>
       <div >
         <img className="h-3"src="data:image/svg+xml;base64,PHN2ZyBjbGFzcz0idGRzLWljb24gdGRzLWljb24tbG9nby13b3JkbWFyayB0ZHMtc2l0ZS1sb2dvLWljb24iIHZpZXdCb3g9IjAgMCAzNDIgMzUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHBhdGggZD0iTTAgLjFhOS43IDkuNyAwIDAgMCA3IDdoMTFsLjUuMXYyNy42aDYuOFY3LjNMMjYgN2gxMWE5LjggOS44IDAgMCAwIDctN0gwem0yMzguNiAwaC02Ljh2MzQuOEgyNjNhOS43IDkuNyAwIDAgMCA2LTYuOGgtMzAuM1Ywem0tNTIuMyA2LjhjMy42LTEgNi42LTMuOCA3LjQtNi45bC0zOC4xLjF2MjAuNmgzMS4xdjcuMmgtMjQuNGExMy42IDEzLjYgMCAwIDAtOC43IDdoMzkuOXYtMjFoLTMxLjJ2LTdoMjR6bTExNi4yIDI4aDYuN3YtMTRoMjQuNnYxNGg2Ljd2LTIxaC0zOHpNODUuMyA3aDI2YTkuNiA5LjYgMCAwIDAgNy4xLTdINzguM2E5LjYgOS42IDAgMCAwIDcgN3ptMCAxMy44aDI2YTkuNiA5LjYgMCAwIDAgNy4xLTdINzguM2E5LjYgOS42IDAgMCAwIDcgN3ptMCAxNC4xaDI2YTkuNiA5LjYgMCAwIDAgNy4xLTdINzguM2E5LjYgOS42IDAgMCAwIDcgN3pNMzA4LjUgN2gyNmE5LjYgOS42IDAgMCAwIDctN2gtNDBhOS42IDkuNiAwIDAgMCA3IDd6IiBmaWxsPSJjdXJyZW50Q29sb3IiLz48L3N2Zz4=" alt="" />
