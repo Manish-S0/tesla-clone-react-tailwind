d1bc0a2d4906ee452d9ced6e93f37fed6ade09ea
manish <manishkrsah88@gmail.com>
2024-07-29 15:00:12 +0545

    add Charging dropdown component

---
+++ src/components/dropdown/Charging.jsx
@@ -0,0 +1,34 @@
+
+import { vehicles, accessories } from '../../utils/constants'
+import Card from '../../card/Card'
+
+const Charging = () => {
+  return (
+    
+    <div>
+      <div className="w-full items-center justify-center mx-auto px-2 sm:px-6 lg:px-8 py-4 flex pt-[50px]">
+            <div className="grid grid-cols-3 md:grid-cols-3 gap-[40px]">
+              {vehicles.map((vehicle) => (
+                <Card
+                  key={vehicle.id}
+                  image={vehicle.image}
+                  title={vehicle.name}
+                  leftButton={vehicle.leftBtn}
+                  rightButton={vehicle.rightBtn}
+                />
+              ))}
+
+            </div>
+            <div className='w-[1px] h-[350px] bg-gray-300 mt-[30px]'></div>
+            <div className="flex flex-col justify-around mt-4 ml-[40px] gap-[15px] w-[196px] font[500]">
+              {accessories.Charging.map((accessory) => (
+                <a href="#" key={accessory} className="text-gray-700 hover:text-black">{accessory}</a>
+              ))}
+              
+            </div>
+          </div>
+    </div>
+  )
+}
+
+export default Charging
